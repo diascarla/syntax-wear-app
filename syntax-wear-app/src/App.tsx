3dbba5fabe596c22cb9e6a41ee56155a6888505e
carla <devcarladias@gmail.com>
2026-01-16 17:57:17 -0300

    feat: adiciona fonte e cores no projeto

--- syntax-wear-app/src/App.tsx
+++ syntax-wear-app/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 
 function App() {
   return (
     <>
-      <p>Ola</p>
+      <p className="bg-error">Ola</p>
     </>
   );
 }
